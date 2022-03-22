73d5b2295d8633f0c41c7f1dac20bd275081d320
SeCre827 <lefterisoikonomy@yahoo.gr>
2022-03-22 15:32:20 +0200

    Built basic Layout

---
+++ json-parser/src/components/Body.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Body.module.css";
+
+const Body = () => {
+  return <div className={classes.contentContainer}>here is your content</div>;
+};
+
+export default Body;
